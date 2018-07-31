883c613158e0d355d4cb99374781d6ea74c093b2
Paul <decouxpaul0@gmail.Com>
2018-07-31 14:04:46 +0200

    First exercices series (Introduction débutants)

---
+++ js/main.js
@@ -0,0 +1,30 @@
+/* Exercice 1
+console.log("Hello world");
+
+var hello = "Hello world";
+console.log(hello);*/
+
+/*Exercice 2
+console.log(3*3);
+console.log(12/0);
+console.log(4+9+78);
+console.log(12-7);
+console.log(5e4);*/
+
+/*Exercice 3
+var nom = prompt("quelle est ton nom ?");
+alert("bonjour" + " " + nom);*/
+
+/*Exercice 4
+var nom="Decoux";
+var prenom="Paul";
+console.log("Bonjour" + " " + prenom + " " + nom);*/
+
+/*Exercice 5
+var a ="123";
+console.log(Number(a));*/
+
+/*Exercice 6
+var string ="Je suis Paul";
+console.log(string.toLowerCase());
+console.log(string.toUpperCase());*/
